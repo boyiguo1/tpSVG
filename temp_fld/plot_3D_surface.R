8b077df57db6adcf9340f3220c35fe0fc3382434
Boyi Guo <boyi.guo.work@gmail.com>
2023-12-07 16:20:18 -0500

    refactor code to print 3D surface.

--- temp_fld/plot_3D_surface.R
+++ temp_fld/plot_3D_surface.R
@@ -13,8 +13,9 @@ spe$total <- counts(spe) |> colSums()
 
 # Smaller set of genes
 idx <- which(
-  rowData(spe)$gene_name %in% c("MOBP", "PCP4", "SNAP25",
-                                "HBB", "IGKC", "NPY")
+  rowData(spe)$gene_name %in%
+    c("MOBP", "PCP4", "SNAP25",
+      "HBB", "IGKC", "NPY")
 )
 spe <- spe[idx, ]
 
@@ -38,51 +39,35 @@ spe$margin_spatialVar <- tmp_pred[, "s(coord_x,coord_y)"] |> exp()
 
 
 # 3D Plot -----------------------------------------------------------------
-
-
 steps <- 50
 coord_x <- with(fit.df, seq(min(coord_x), max(coord_x), length = steps) )
 coord_y <-  with(fit.df, seq(min(coord_y), max(coord_y), length = steps) )
 newdat <- expand.grid(coord_x = coord_x, coord_y = coord_y)
 surf_df <- matrix(predict(tp_mdl, newdat), steps, steps)
 
-
-# Now plot it
-
-Lab.palette <- colorRampPalette(c("red", "orange", "blue"),
-                                space = "Lab")
-
-x <- seq(-1.95, 1.95, length.out = 30)
-y <- seq(-1.95, 1.95, length.out = 35)
-z <- outer(x, y, function(a, b) a*b^2)
+z <- surf_df
 nrz <- nrow(z)
 ncz <- ncol(z)
 
 nbcol <- 100
+
+library(viridis)
+Lab.palette <- colorRampPalette(viridis(nbcol))
 color <- Lab.palette(nbcol)
 # Compute the z-value at the facet centres
 zfacet <- z[-1, -1] + z[-1, -ncz] + z[-nrz, -1] + z[-nrz, -ncz]
-# zfacet <- surf_df[-1, -1] + surf_df[-1, -ncz] + surf_df[-nrz, -1] + surf_df[-nrz, -ncz]
+
 # Recode facet z-values into color indices
 facetcol <- cut(zfacet, nbcol)
 
-png("inst/figures/tpSVG_raw.png")
-persp(coord_x, coord_y, surf_df,
-           col = color[facetcol], theta = 45,
-           border = NA,
-           box = FALSE)
-dev.off()
+persp(
+  coord_x, coord_y, surf_df,
+  col = color[facetcol], theta = 45,
+  border = NA,
+  box = FALSE
+)
 
 
-library(hexSticker)
-imgurl <- here::here("inst/figures/tpSVG_raw.png")
-sticker(imgurl,
-        package="tpSVG",
-        h_fill = "white",
-        p_size=20, p_color = "black", p_y = 0.4,
-        s_x=0.9, s_y=1,
-        s_width=1, s_height=1,
-        filename="inst/figures/hexgon_sticker.png")
 
 
 
